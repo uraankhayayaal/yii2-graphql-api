f7b59bc55d1573f11bf696a0d360bfa90626b6be
uraankhayayaal <uraankhayayaal@gmail.com>
2019-09-26 13:03:24 +0900

    faker added

--- commands/FakerController.php
+++ commands/FakerController.php
@@ -7,16 +7,52 @@
 
 namespace app\commands;
 
+use phpDocumentor\Reflection\Types\Boolean;
 use yii\console\Controller;
 use yii\console\ExitCode;
 
 class FakerController extends Controller
 {
+    public const postsCount = 100;
+    public const commentsCount = 12;
+    public $faker = null;
+
     public function actionIndex($message = 'hello world')
     {
-        $faker = \Faker\Factory::create();
-        echo $faker->name . "\n";
+        $this->faker = \Faker\Factory::create();
+
+        $this->generatePosts();
 
         return ExitCode::OK;
+    }
+
+    protected function generatePosts(){
+        for ($i=0; $i < self::postsCount; $i++) { 
+            $model = new \app\models\Post();
+            $model->title = $this->faker->sentences($nb = 1, $asText = true);
+            $model->body = $this->faker->realText($maxNbChars = rand(400,700), $indexSize = rand(2,5));
+            $model->is_public = 1;
+            $model->created_at = time();
+            if($model->save()){
+                echo "New Post: ".$model->id."\n";
+                $this->generateComments($model->id);
+            }else{
+                echo "Error: ".var_dump($model->errors);
+            };
+        }
+    }
+
+    protected function generateComments($post_id = 0){
+        for ($i=0; $i < self::commentsCount; $i++) { 
+            $model = new \app\models\Comment();
+            $model->text = $this->faker->text($maxNbChars = rand(100,255));
+            $model->post_id = $post_id;
+            $model->created_at = time();
+            if($model->save()){
+                echo "New Comment: ".$model->id."\n";
+            }else{
+                echo "Error: ".var_dump($model->errors);
+            };
+        }
     }
 }
